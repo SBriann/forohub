52e4800229bc38a827799f817885b991341e90cc
Brian <briann.sandovalm@gmail.com>
2025-01-15 16:13:45 -0500

    Creación repositorio de usuarios

---
+++ src/main/java/com/alura/forohub/repository/UsuarioRepository.java
@@ -0,0 +1,9 @@
+package com.alura.forohub.repository;
+
+import com.alura.forohub.model.entity.Usuario;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+
+public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
+    UserDetails findByLogin(String username);
+}
