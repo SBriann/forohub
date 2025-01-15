17df49a0044cc4b441094b0a8f6f8f7878f8bcfc
Brian <briann.sandovalm@gmail.com>
2025-01-15 14:48:27 -0500

    Configuración de seguridad para probar la API

---
+++ src/main/java/com/alura/forohub/infra/security/SecurityConfig.java
@@ -0,0 +1,30 @@
+package com.alura.forohub.infra.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(authorize -> authorize
+                        .requestMatchers("/topicos", "/topicos/**").permitAll()
+                        .anyRequest().authenticated()
+                )
+                .formLogin(formLogin -> formLogin
+                        .loginPage("/login")
+                        .permitAll()
+                )
+                .rememberMe(Customizer.withDefaults());
+
+        return http.build();
+    }
+}
